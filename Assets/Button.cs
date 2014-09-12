6cf901f83aeef589399276a4615c474fcd33caef
sequreExam <sequres.true.colors@gmail.com>
2014-09-13 03:40:54 +0900

    とりま
    こぴーした

---
+++ Assets/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : TouchScript {
+	public GameObject text;
+	public GameObject cam;
+	public int test;
+	// Use this for initialization
+	void Start () {
+
+		test = 1;
+
+		//cam = GameObject.Find (MainCamera);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnButton( int a, int b ){
+
+	}
+}
